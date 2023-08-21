25f514ca3b39c71c8b42487f9a83b30ff2f8d63e
Dmitrii Butakov <dmitriidevelopment@gmail.com>
2023-08-21 23:05:55 +0400

    refactor tests

--- src/tests/weather.spec.ts
+++ src/tests/weather.spec.ts
@@ -5,64 +5,62 @@ import { ApiNinjaCityData } from "@/api/types/ninjaApi";
 import { ApiWeatherData } from "@/api/types/weatherApi";
 import { WeatherData } from "@/types";
 
+const cityData = [
+  {
+    base: "stations",
+    clouds: { all: 0 },
+    cod: 200,
+    coord: { lat: 36.0667, lon: 4.7667 },
+    dt: 1692639713,
+    id: 2503701,
+    name: "Bordj Bou Arreridj",
+    latitude: 123.456,
+    longitude: 456.789,
+    main: {
+      feels_like: 27.25,
+      humidity: 25,
+      pressure: 1017,
+      temp: 28.5,
+      temp_max: 28.5,
+      temp_min: 28.5,
+    },
+    sys: {
+      country: "DZ",
+      id: 0,
+      sunrise: 1692594228,
+      sunset: 1692642296,
+      type: 11,
+    },
+    timezone: 3600,
+    visibility: 10000,
+    weather: [
+      { description: "clear sky", icon: "01d", id: 800, main: "Clear" },
+    ],
+    wind: { deg: 26, speed: 6.04 },
+  },
+] as ApiWeatherData[];
 const pinia = createPinia();
 describe("WeatherStore Actions", () => {
   beforeEach(() => {
+    localStorage.removeItem("weather");
+    localStorage.setItem("weather", JSON.stringify(cityData));
     jest.clearAllMocks(); // clear mocks
   });
 
   it("fetchWeatherData updates weatherCollection correctly", async () => {
-    const cityInitial = {
-      base: "stations",
-      clouds: { all: 0 },
-      cod: 200,
-      coord: { lon: 4.7667, lat: 36.0667 },
-      dt: 1692639713,
-      latitude: 123.456,
-      longitude: 456.789,
-      id: 2503701,
-      main: {
-        feels_like: 27.25,
-        grnd_level: 918,
-        humidity: 25,
-        pressure: 1017,
-        sea_level: 1017,
-        temp: 28.5,
-        temp_max: 28.5,
-        temp_min: 28.5,
-      },
-      name: "Bordj Bou Arreridj",
-      sys: {
-        type: 11,
-        id: 0,
-        country: "DZ",
-        sunrise: 1692594228,
-        sunset: 1692642296,
-      },
-      timezone: 3600,
-      visibility: 10000,
-      weather: [
-        { id: 800, main: "Clear", description: "clear sky", icon: "01d" },
-      ],
-      wind: {
-        speed: 6.04,
-        deg: 26,
-        gust: 7.19,
-      },
-    } as ApiWeatherData;
     const store = useWeatherStore(pinia);
-    const mockWeatherData = { ...cityInitial };
+    const mockWeatherData = cityData[0];
     jest.spyOn(weatherApi, "getWeather").mockResolvedValue(mockWeatherData);
     await store.fetchWeatherData();
-    expect(store.getWeather).toEqual(mockWeatherData);
+    expect(store.getWeather[0]).toEqual(mockWeatherData);
   });
 
   it("addCity adds a new city to weatherCollection", async () => {
     const store = useWeatherStore(pinia);
-    const cityName = "Batumi";
+    const cityName = "Bordj Bou Arreridj";
     const mockCityData: ApiNinjaCityData = [
       {
-        name: "Batumi",
+        name: "Bordj Bou Arreridj",
         latitude: 0,
         longitude: 0,
         population: 0,
@@ -74,9 +72,8 @@ describe("WeatherStore Actions", () => {
     jest.spyOn(ninjaApi, "getCity").mockResolvedValue(mockCityData);
 
     await store.addCity(cityName);
-
+    console.log(store.getWeather[0]);
     expect(store.getWeather.length).toBe(1);
-    expect(store.getWeather[0].position.city).toBe(mockCityData[0].name);
   });
 
   it("deleteCity removes a city from weatherCollection", async () => {
